ee486238bc8ee901d561379bfac929f4d722a71b
Ash <ash.tappin@blackpepper.co.uk>
2020-07-29 11:04:13 +0100

    Create YesNo subclass

---
+++ CustomTypeclasses/yesNo.hs
@@ -0,0 +1,13 @@
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where 
+	yesno 0 = False
+	yesno _ = True
+
+instance YesNo Bool where 
+	yesno a = a
+
+instance YesNo [a] where
+	yesno [] = False
+	yesno _ = True 
